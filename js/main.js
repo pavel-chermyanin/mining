447b3b1cd0952202ca83b6e91aab7454a80badfc
Pavel Che <alien.killer3@bk.ru>
2022-02-01 21:36:17 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -4,4 +4,25 @@ $(function () {
 
     $('.filter-style').styler();
 
+
+    // function languageWord () {
+    //     let select_value = $('.jq-selectbox__select-text')[0].innerText;
+    //     $('.jq-selectbox__select-text')[0].innerText = select_value.slice(0, 2)
+    //     console.log(test)
+    // }
+    // languageWord();
+
+    // $('.language-btn').addEventListener('change', function() {
+    //     console.log('111')
+    // })
+
+
+    $('.slider-box').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slider__btn slider__btn-prev"><img src="./images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="slider__btn slider__btn-next"><img src="./images/arrow-right.svg" alt=""></button>',
+    })
+
+
 });
